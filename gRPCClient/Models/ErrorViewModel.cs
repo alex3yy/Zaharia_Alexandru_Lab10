17d18724025ac110f9fccee65525f96a9bec8e22
Alex Zaharia <alex_zaharia10@yahoo.com>
2021-12-09 20:44:13 +0200

    Add all related files of lab10

---
+++ gRPCClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace gRPCClient.Models {
+    public class ErrorViewModel {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
